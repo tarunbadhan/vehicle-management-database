3a0e6642231e2a69a6ed2bfb6a6be12a1eeb865d
tarunbadhan <tarunbadhan116@gmail.com>
2026-04-05 18:40:54 -0300

    Added insert.sql

---
+++ README.md
@@ -0,0 +1,22 @@
+\# Vehicle Management Database System
+
+
+
+\## Description
+
+This project is a database system designed to manage vehicles, their owners, and service records.
+
+
+
+\## Technologies Used
+
+\- SQL (SQLite)
+
+\- Java (JDBC)
+
+
+
+\## Author
+
+Tarun Badhan
+
---
+++ insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO Owner VALUES
+(1, 'Tarun Badhan', '506-111-2222', 'tarun@email.com', 'Saint John, NB'),
+(2, 'Alex Sharma', '506-333-4444', 'alex@email.com', 'Fredericton, NB');
+
+INSERT INTO Vehicle VALUES
+(101, 1, 'NBX123', 'Toyota', 'Corolla', 2020, 'Black'),
+(102, 1, 'NBY456', 'Honda', 'Civic', 2022, 'White'),
+(103, 2, 'NBZ789', 'Ford', 'Escape', 2019, 'Blue');
+
+INSERT INTO ServiceRecord VALUES
+(1001, 101, '2026-03-01', 'Oil Change', 79.99, 'Engine oil and filter changed'),
+(1002, 101, '2026-03-15', 'Brake Inspection', 49.99, 'Front brake pads checked'),
+(1003, 103, '2026-03-20', 'Tire Rotation', 39.99, 'All four tires rotated');
